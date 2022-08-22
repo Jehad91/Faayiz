8aae203a6718d59a7815e8b158b4aa289ec29fd5
Jehad91 <j.abushaqra1@gmail.com>
2022-08-22 16:09:47 +0300

    add form buttom

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import { Button } from "antd";
 
 export default function Home() {
   const { name } = useRouter().query;
@@ -20,6 +22,10 @@ export default function Home() {
             {!name ? "Next.js!" : name}
           </a>
         </h1>
+
+        <Button type="primary" className={styles.formButton}>
+          <Link href="/form">Form</Link>
+        </Button>
 
         <p className={styles.description}>
           Get started by editing{" "}
